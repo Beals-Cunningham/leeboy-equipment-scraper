811edbe05de49bdf67cb35870624d8ff902287a6
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-08-22 11:17:05 -0500

    all asphalt pavers

--- web/js/pages.js
+++ web/js/pages.js
@@ -3,6 +3,14 @@ let pages =
     {"url": "https://www.leeboy.com/products/1000g-leeboy-asphalt-paver/", "flavor":"leeboy"},
     {"url": "https://www.leeboy.com/products/5300b-leeboy-asphalt-paver/", "flavor":"leeboy"},
     {"url": "https://www.leeboy.com/products/6150-leeboy-asphalt-paver/", "flavor":"leeboy"},
+    {"url": "https://www.leeboy.com/products/7000c-leeboy-asphalt-paver/", "flavor":"leeboy"},
+    {"url": "https://www.leeboy.com/products/8500e/", "flavor":"leeboy"},
+    {"url": "https://www.leeboy.com/products/8515e-leeboy-asphalt-paver/", "flavor":"leeboy"},
+    {"url": "https://www.leeboy.com/products/8520c-leeboy-asphalt-paver/", "flavor":"leeboy"},
+    {"url": "https://www.leeboy.com/products/8530-leeboy-asphalt-paver/", "flavor":"leeboy"},
+    {"url": "https://www.leeboy.com/products/8608-leeboy-asphalt-paver/", "flavor":"leeboy"},
+    {"url": "https://www.leeboy.com/products/8616d-leeboy-asphalt-paver/", "flavor":"leeboy"},
+    
 
 ]
 
